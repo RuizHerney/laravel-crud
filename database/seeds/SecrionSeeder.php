f7f8910c31aa28450f26bd8f1fe1552c5f9369d7
Herney Ruiz <herneyruiz36@gmail.com>
2019-11-09 12:34:38 -0500

    start project

---
+++ database/seeds/SecrionSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SecrionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
