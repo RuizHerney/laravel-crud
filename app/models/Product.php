0b38ba7cc6b2fe284352df300663b611c498e73d
Herney Ruiz <herneyruiz36@gmail.com>
2019-11-11 10:27:10 -0500

    creation the models Product, Section and State

--- app/models/Product.php
+++ app/models/Product.php
@@ -1,10 +1,29 @@
 <?php
 
-namespace App;
+namespace App\models;
+
+use App\Models\Section;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+
+        'name',
+        'price',
+        'country_origin',
+        'section_id',
+        'state_id'
+    ];
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
 }
---
+++ app/models/State.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class State extends Model
+{
+    protected $fillacle = [
+        'name'
+   ];
+}
