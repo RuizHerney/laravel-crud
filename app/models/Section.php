72bb6bbbd03e728d82fdc0df80e82d3c6678a4e4
Herney Ruiz <herneyruiz36@gmail.com>
2019-11-11 10:32:26 -0500

    renamber fillable

--- app/models/Section.php
+++ app/models/Section.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Section extends Model
 {
-   protected $fillacle = [
+   protected $fillable = [
         'name'
    ];
 }
--- app/models/State.php
+++ app/models/State.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    protected $fillacle = [
+    protected $fillable = [
         'name'
    ];
 }
